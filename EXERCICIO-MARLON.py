29554d0c8261157ffdb887d76e7da60d70dfc975
Bruna Lima <brunaalimas@gmail.com>
2021-09-25 19:18:13 -0300

    Arquivo adicionado

---
+++ EXERCICIO-MARLON.py
@@ -0,0 +1,28 @@
+# 2a) Faça um Programa que converta metros para números
+
+# metros = int(input('Insira os metros a serem convertidos em centímetros: '))
+# centimetros = metros * 100
+# print(centimetros , 'centimetros')
+
+# 2b) Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo inválido.
+
+# sexo = str(input('Qual o seu sexo? M ou F? : ').upper())
+# if sexo == 'M':
+#     print('Masculino')
+# elif sexo == 'F':
+#     print('Feminino')
+# else:
+#     print('Sexo inválido')
+
+# 2c) Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário
+#  monstrando uma mensagem de erro e voltando a pedir as informações.
+# print('Olá, seja bem-vindo. Vamos começar o seu cadastro?')
+# usuario = (input('Digite seu primeiro nome: '))
+# senha = (input('Digite uma senha: '))
+# while usuario == senha:
+#     print('ERRO: Digite uma senha que não contenha seu nome, tente novamente')
+#     usuario = (input('Digite seu primeiro nome: '))
+#     senha = input('Digite uma senha: ')
+# else:
+#     print('Estamos felizes em tê-lo (a) conosco. Cadastro realizado com sucesso!')  
+    
